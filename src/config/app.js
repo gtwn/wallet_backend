c3c156eb326dda0d6c2defb16e4b51a751cd76c8
unknown <jiragrid18@gmail.com>
2019-11-26 21:53:05 +0700

    update

---
+++ src/config/app.js
@@ -0,0 +1,11 @@
+module.exports = {
+    port: 3000,
+    host: "localhost",
+    tokenExpire: false,
+    schemes: "http",
+    basePath: "/",
+    salt: 10,
+    swaggerUIPath: "/swaggerui/",
+    documentationPath: "/documentation",
+    jsonPath: "/swagger.json"
+}
